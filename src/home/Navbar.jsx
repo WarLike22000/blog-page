2188afe7af4102ec97dd6b1cf93a1afabf8a59d6
Alireza Kamali <kamalialireza788@gmail.com>
2023-05-25 16:19:54 +0330

    add collapse in hamburger menu

--- src/home/Navbar.jsx
+++ src/home/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { Divider, Drawer } from '@mui/material';
+import { Divider, Drawer, Collapse } from '@mui/material';
 //assets
 import arrowDown from '../assets/chevron-down-svgrepo-com.svg';
 import menu from '../assets/align-right-svgrepo-com.svg';
@@ -22,27 +22,106 @@ const Navbar = () => {
     const [open, setOpen] = useState(false);
     const [openProduct, setOpenProduct] = useState(false);
     const [openResources, setOpenResources] = useState(false);
+
+    const [clickProduct, setClickProduct] = useState(false)
+    const [clickResources, setClickResources] = useState(false)
+    
+    const closeHandler = () => {
+        setOpen(false)
+        setClickProduct(false)
+        setClickResources(false)
+    }
     
     return (
         <div className='sticky top-0 z-10 backdrop-blur bg-white/60 flex justify-between items-center py-5'>
-            <Drawer className='backdrop-blur-md' open={open} onClose={() => setOpen(false)} anchor='top'>
+            <Drawer className='backdrop-blur-md' open={open} onClose={closeHandler} anchor='top'>
                 <div className='py-5'>
                     <div onClick={() => setOpen(false)} className='mx-4 py-2 hover:bg-sky-100 transition-all cursor-pointer px-3 rounded'>
                         <Link to='#'>
                             <p className='font-medium'>Home</p>
                         </Link>
                     </div>
-                    <div onMouseEnter={() => setOpenProduct(true)} onMouseLeave={() => setOpenProduct(false)} className='mx-4 py-2 hover:bg-sky-100 transition-all cursor-pointer px-3 rounded'>
+                    <div onClick={() => setClickProduct(!clickProduct)} onMouseEnter={() => setOpenProduct(true)} onMouseLeave={() => setOpenProduct(false)} className='mx-4 py-2 hover:bg-sky-100 transition-all cursor-pointer px-3 rounded'>
                         <Link to='#'  className='flex items-center'>
                             <p className='font-medium'>Products</p>
-                            <img className='w-3 ml-2' src={arrowDown} alt='arrDown' />
+                            <img className={`w-3 ml-2 ${clickProduct && 'rotate-180'} transition-all`} src={arrowDown} alt='arrDown' />
                         </Link>
+                        <Collapse in={clickProduct}>
+                            <div className='collapseCSS'>
+                                <div>
+                                    <nav>
+                                        <img src={overview} alt='overview' />
+                                        <p>Overview</p>
+                                    </nav>
+                                    <span>You can use other facilities.</span>
+                                </div>
+                                <Divider variant='fullWidth' />
+                                <div>
+                                    <nav>
+                                        <img src={features} alt='features' />
+                                        <p>Features</p>
+                                    </nav>
+                                    <span>You can use other facilities.You can use other facilities.</span>
+                                </div>
+                                <Divider variant='fullWidth' />
+                                <div>
+                                    <nav>
+                                        <img src={solutions} alt='solutions' />
+                                        <p>Solutions</p>
+                                    </nav>
+                                    <span>You can use other facilities.</span>
+                                </div>
+                                <Divider variant='fullWidth' />
+                                <div>
+                                    <nav>
+                                        <img src={tutorials} alt='tutorials' />
+                                        <p>Tutorials</p>
+                                    </nav>
+                                    <span>You can use other facilities.You can use other facilities.</span>
+                                </div>
+                            </div>
+                        </Collapse>
                     </div>
-                    <div onMouseEnter={() => setOpenResources(true)} onMouseLeave={() => setOpenResources(false)} className='mx-4 py-2 hover:bg-sky-100 transition-all cursor-pointer px-3 rounded'>
+                    <div onClick={() => setClickResources(!clickResources)} onMouseEnter={() => setOpenResources(true)} onMouseLeave={() => setOpenResources(false)} className='mx-4 py-2 hover:bg-sky-100 transition-all cursor-pointer px-3 rounded'>
                         <Link to='#'  className='flex items-center'>
                             <p className='font-medium'>Resources</p>
-                            <img className='w-3 ml-2' src={arrowDown} alt='arrDown' />
+                            <img className={`w-3 ml-2 ${clickResources && 'rotate-180'} transition-all`} src={arrowDown} alt='arrDown' />
                         </Link>
+                        <Collapse in={clickResources}>
+                        <div className='collapseCSS'>
+                            <div>
+                                <nav>
+                                    <img src={blog} alt='blog' />
+                                    <p>Blog</p>
+                                </nav>
+                                <span>You can use other facilities.</span>
+                            </div>
+                            <Divider variant='fullWidth' />
+                            <div>
+                                <nav>
+                                    <img src={newsletter} alt='newsletter' />
+                                    <p>Newsletter</p>
+                                </nav>
+                                <span>You can use other facilities.You can use other facilities.</span>
+                            </div>
+                            <Divider variant='fullWidth' />
+                            <div>
+                                <nav>
+                                    <img src={events} alt='events' />
+                                    <p>Events</p>
+                                </nav>
+                                <span>You can use other facilities.</span>
+                            </div>
+                            <Divider variant='fullWidth' />
+                            <div>
+                                <nav>
+                                    <img src={helpCentre} alt='helpCentre' />
+                                    <p>Help Centre</p>
+                                </nav>
+                                <span>You can use other facilities.You can use other facilities.</span>
+                            </div>
+                        </div>
+                        </Collapse>
                     </div>
                     <div onClick={() => setOpen(false)} className='mx-4 py-2 hover:bg-sky-100 transition-all cursor-pointer px-3 rounded'>
                         <Link to='#'>
